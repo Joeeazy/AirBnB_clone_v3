b6f407f7d2c1ebf055995145e54ddf3bcad41d2b
Joeeazy <joeeazy34@gmail.com>
2024-02-26 08:39:46 +0300

    updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,23 +14,18 @@ from models.user import User
 
 @app_views.route('/status')
 def get_status():
-    '''Gets the status of the API.
-    '''
+    """Returns JSON file"""
     return jsonify(status='OK')
 
 
 @app_views.route('/stats')
 def get_stats():
-    '''Gets the number of objects for each type.
-    '''
-    objects = {
-        'amenities': Amenity,
-        'cities': City,
-        'places': Place,
-        'reviews': Review,
-        'states': State,
-        'users': User
-    }
-    for key, value in objects.items():
-        objects[key] = storage.count(value)
-    return jsonify(objects)
+    """Returns type of each object."""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
